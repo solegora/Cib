4a035cdcf32fbfc30b643d2c7e5f8c87e44e9e34
Oliver <oliver.legora@siemens.com>
2021-05-07 18:26:21 +0200

    CIB : Init front end project

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// tabs
+
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var booking = btnTarget.dataset.booking;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + booking).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
